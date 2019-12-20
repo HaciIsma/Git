2ddb52c687a1316fc6c2594eaca13207bf7541b1
HaciIsma <haciismayilov5@outlook.com>
2019-12-20 12:32:51 +0400

    last

--- Git.cs
+++ Git.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp11
 {
-   static class Git
+    static class Git
     {
         public static void Go()
         {
@@ -18,7 +14,7 @@ namespace ConsoleApp11
             commit = Console.ReadLine();
             commit = "\'" + commit + "\'";
             string remoteControl = "https://github.com/HaciIsma/Git.git";
-            string command = $"cd..; cd..; git init; git commit -m {commit};git add .;git remote add origin {remoteControl} ;git push -u origin master;pause";
+            string command = $"cd..; cd..; git init;git add .; git commit -m {commit};git remote add origin {remoteControl} ;git push -u origin master;pause";
             process.StartInfo = new ProcessStartInfo(destination, command);
             process.Start();
         }

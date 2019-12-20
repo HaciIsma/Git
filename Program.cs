fcfe42cf74ea7caf36c5acb46907b6051286823a
HaciIsma <haciismayilov5@outlook.com>
2019-12-20 08:35:26 +0400

    3

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace Git
             //string remote = "git remote add origin https://github.com/HaciIsma/Git.git;";
             //string push = "git push -u origin master;";
 
-            string commit = "\'Second commit\'";
+            string commit = "3";
             commit = "\'" + commit + "\'";
             string remoteControl = "https://github.com/HaciIsma/Git.git";
             string command = $"cd..; cd..; git init; git commit -m {commit};git add .;git remote add origin {remoteControl} ;git push -u origin master;pause";

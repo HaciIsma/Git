589fc3b6debfcb3b9851d913d2cc19a76f997085
HaciIsma <haciismayilov5@outlook.com>
2019-12-20 08:40:39 +0400

    6

--- Program.cs
+++ Program.cs
@@ -13,7 +13,9 @@ namespace Git
         {
             Process process = new Process();
             string destination = "Powershell.exe";
-            string commit = "4";
+            string commit = default;
+            Console.Write("Enter commit: ");
+            commit = Console.ReadLine();
             commit = "\'" + commit + "\'";
             string remoteControl = "https://github.com/HaciIsma/Git.git";
             string command = $"cd..; cd..; git init; git commit -m {commit};git add .;git remote add origin {remoteControl} ;git push -u origin master;pause";

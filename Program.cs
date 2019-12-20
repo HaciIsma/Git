abab8c5afa1cb02320306a7cf5eed3062989ab65
HaciIsma <haciismayilov5@outlook.com>
2019-12-20 07:46:59 +0400

    first

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            string destination = "Powershell.exe";
+            string commit = "first commit";
+            commit = "\"" + commit + "\"";
+            string remoteControl = "https://github.com/HaciIsma/Git.git;git";
+            string commants = $@"cd ..; cd ..;
+                                 git init;
+                                 git add .;
+                                 git commit -m {commit}; 
+                                 git remote add origin {remoteControl}; 
+                                 git push -u origin master;
+                                 pause;";
+
+            process.StartInfo = new ProcessStartInfo(destination, commants);
+            process.Start();
+        }
+    }
+}
